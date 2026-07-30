0bc40e4c382f12233a5adb26b86091e3191c4558
calsup7 <calsup@clarkinc.biz>
2026-07-30 16:53:50 -0700

    Update watch_folder_start.ps1

--- watch_folder_start.ps1
+++ watch_folder_start.ps1
@@ -1,1 +1,2 @@
-C:\work\Video-AI-Transcribing-v2\.venv\Scripts\python.exe C:\work\Video-AI-Transcribing-v2\watch_folder.py
+C:\work\Video-AI-Transcribing-v2\.venv\Scripts\python.exe pyinstaller --onefile --runtime-tmpdir=. --hidden-import win32timezone watch_folder.py # Creates exe file
+watch_folder.py install # Installs service
